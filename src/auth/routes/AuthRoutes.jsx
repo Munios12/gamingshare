f7c13845ca28a8ed7ea6f8658c032cc53701b705
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2022-09-02 17:37:32 +0200

    Changes to production

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage, RegisterPage } from "../pages";
+import { Login } from "../../components/login/Login";
+import { Register } from "../../components/register/Register";
 
 export const AuthRoutes = () => {
   return (
     <Routes>
-      <Route path="login" element={<LoginPage />} />
-      <Route path="register" element={<RegisterPage />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
 
       <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
